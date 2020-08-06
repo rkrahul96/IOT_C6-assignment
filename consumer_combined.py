7f743d5ca900f4a7d82b6a51cf6473d68bb09b7d
rkrahul96 <65348851+rkrahul96@users.noreply.github.com>
2020-08-06 20:55:49 +0530

    by rahul

--- consumer_combined.py
+++
@@ -1,7 +0,0 @@
-from kafka import KafkaConsumer
-consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
-                         auto_offset_reset='earliest',
-                         consumer_timeout_ms=1000)
-consumer.subscribe(['customer_data'])
-for message in consumer:
-     print(message)
